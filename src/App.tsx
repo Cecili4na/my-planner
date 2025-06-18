62f0e83af142989daa3846e9fb14e3ba4db13630
Ana Oliveira <ana@Cecis-MacBook-Pro.local>
2025-06-18 10:07:28 -0300

    corercoes versoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Expenses from './pages/Expenses';
 import Schedule from './pages/Schedule';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'expenses',
-        element: <Expenses />,
-      },
-      {
-        path: 'schedule',
-        element: <Schedule />,
-      },
-    ],
-  },
-]);
-
 const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="schedule" element={<Schedule />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App; 
